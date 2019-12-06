4ecdadb9777d83fdd6959edba6619fd31347cbbd
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2019-12-06 12:06:58 -0500

    tabs added

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,4 +8,22 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+function Topic(topicData){
+    newTopic = document.createElement('div');
+    newTopic.classList.add('tab');
+    newTopic.textContent = topicData;
 
+    return newTopic;
+}
+
+const topicsCont = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(resonse => {
+        resonse.data.topics.forEach(topic => {
+            topicsCont.appendChild(Topic(topic));
+        })
+    })
+    .catch(err => {
+        console.log("got an error: ", error);
+    })
