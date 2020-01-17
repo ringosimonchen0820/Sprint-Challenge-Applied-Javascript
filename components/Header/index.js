4d5bf75e55253ebfa34faa3bb5a61edc8fd9593d
SimonChen <ringosimonchen0820@gmail.com>
2020-01-17 10:26:19 -0500

    setup header layout

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,6 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {
-    
+function Header(dateData, titleData, tempData) {
+    // create elements 
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp =document.createElement('span');
+
+    // setup element layout 
+    header.append(date);
+    header.append(title);
+    header.append(temp);
+
+    // assign class to elements 
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // add content to elements
+    date.textContent = dateData;
+    title.textContent = titleData;
+    temp.textContent = tempData;
+
+    // return result(parent div)
+    return header;
 }
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.append(Header('SMARCH 28, 2019', 'Lambda Times', '98°'));
